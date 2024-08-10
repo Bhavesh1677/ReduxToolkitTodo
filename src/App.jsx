6ed24f5c5f714efded5ec91a04cc8b8deebf3c5c
Bhavesh Pawar <pawarbhavesh16@gmail.com>
2024-08-10 12:20:38 +0530

    AddTodo and Todos components are imported and wrapped in the provider

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,20 @@
 import React from 'react'
+import AddTodo from './components/AddTodo'
+import Todos from './components/Todos'
+import { Provider } from 'react-redux'
+import {store} from './app/store'
 
 function App() {
   
 
   return (
-    <>
-      
-    </>
+    <Provider store={store}>
+    <div className='flex flex-col items-center w-[100vw] h-[100vh] bg-slate-700'>
+      <h1 className='text-white text-2xl my-5'>Todo List</h1>
+      <AddTodo/>
+      <Todos/>
+    </div>
+    </Provider>
   )
 }
 
