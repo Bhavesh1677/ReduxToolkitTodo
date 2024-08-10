fc806708fd52874a90fe5330406bdbfcbb576f70
Bhavesh Pawar <pawarbhavesh16@gmail.com>
2024-08-10 12:21:06 +0530

    sum styling did

--- src/components/Todos.jsx
+++ src/components/Todos.jsx
@@ -8,14 +8,14 @@ function Todos() {
 
   return (
     <>
-    <div>Todos</div>
-    <ul className="list-none">
+    <div className='text-white text-xl mt-5 mb-3'>Todos</div>
+    <ul className="list-none w-[50vw]">
         {todos.map((todo) => (
           <li
-            className="mt-4 flex justify-between items-center bg-zinc-800 px-4 py-2 rounded"
+            className='bg-slate-900 flex justify-between items-center text-white px-5 py-2 my-5 rounded'
             key={todo.id}
           >
-            <div className='text-white'>{todo.text}</div>
+            <div className=''>{todo.text}</div>
             <button
              onClick={() => dispatch(removeTodo(todo.id))}
               className="text-white bg-red-500 border-0 py-1 px-4 focus:outline-none hover:bg-red-600 rounded text-md"
